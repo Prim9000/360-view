69ef26c2dbb2a1829545ca840a506ed071a3b6b3
PrimWong <65888725+Prim9000@users.noreply.github.com>
2025-05-01 20:04:09 +0700

    update files

---
+++ data.js
@@ -0,0 +1,360 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-welcome-to-ban-mae-hat",
+      "name": "Welcome to Ban Mae Hat",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.648731554094681,
+          "pitch": 0.08188745180011026,
+          "rotation": 0,
+          "target": "1-karen-house"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.8549318125779433,
+          "pitch": 0.25121651289934555,
+          "title": "Welcome to Ban Mae Hat",
+          "text": "<span style=\"font-size: 16px; background-color: rgba(103, 115, 131, 0.8);\">Organic lifestyle of Karen People. Surrounded by mountains, the lifestyle of Karen people are connected to nature, water, forest and agriculture.</span>"
+        }
+      ]
+    },
+    {
+      "id": "1-karen-house",
+      "name": "Karen House",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5172660491946388,
+          "pitch": 0.16123916049079234,
+          "rotation": 0,
+          "target": "2-natural-environment"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.016683642198541904,
+          "pitch": 0.17266798489952428,
+          "title": "Karen House",
+          "text": "Karen people made their house from wood, and natural material. They live together in a family and cook food."
+        }
+      ]
+    },
+    {
+      "id": "2-natural-environment",
+      "name": "Natural Environment",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.44362575060258536,
+          "pitch": 0.1857102937947488,
+          "rotation": 0,
+          "target": "3-tank-and-hill-top"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-tank-and-hill-top",
+      "name": "Tank and Hill top",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 1.141681072000079,
+        "pitch": 0.15493638825274658,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.10102475133379585,
+          "pitch": 0.086358813977359,
+          "rotation": 0,
+          "target": "4-surrounding-mountains"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.9099084751448441,
+          "pitch": -0.030817306556974344,
+          "title": "Welcome to Ban Mae Hat",
+          "text": "<span style=\"font-size: 16px; background-color: rgba(103, 115, 131, 0.8);\">Organic lifestyle of Karen People. Surrounded by mountains, the lifestyle of Karen people are connected to nature, water, forest and agriculture.</span>"
+        },
+        {
+          "yaw": -0.4901466623173292,
+          "pitch": 0.007076682803008438,
+          "title": "Water Tank",
+          "text": "Infrastructure and water built by the Karen Hill Tribe trust."
+        }
+      ]
+    },
+    {
+      "id": "4-surrounding-mountains",
+      "name": "Surrounding Mountains",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 1.6036530861657452,
+        "pitch": 0.07351139039192844,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.572475572733083,
+          "pitch": 0.2556815416482703,
+          "rotation": 0,
+          "target": "5-irrigation-field"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.9200022264678172,
+          "pitch": 0.11319061427576571,
+          "title": "Water Tank",
+          "text": "This tank can hold 2000 Liters of water for the family to use."
+        }
+      ]
+    },
+    {
+      "id": "5-irrigation-field",
+      "name": "Irrigation Field",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1562.5,
+      "initialViewParameters": {
+        "yaw": -0.39473408952189537,
+        "pitch": -0.018744700515274104,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.898795576744515,
+          "pitch": 0.055339562786636876,
+          "rotation": 0,
+          "target": "6-baan-na-ngew"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.38615865015808026,
+          "pitch": 0.1168023889601173,
+          "title": "Irrigation Field",
+          "text": "<p style=\"font-size: 16px\" data-pm-slice=\"1 1 []\">Agriculture has become the main part of Karen's lifestyle. They plant food, grow crops, rice, and vegetables.</p>"
+        },
+        {
+          "yaw": -2.351862225470219,
+          "pitch": -0.009114009875119677,
+          "title": "Water pipe",
+          "text": "Emergence of PVC Water Pipes.&nbsp;These plastic pipes are joined with solvent-weld fittings or compression couplings."
+        }
+      ]
+    },
+    {
+      "id": "6-baan-na-ngew",
+      "name": "Baan Na Ngew",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -1.9539193532967367,
+        "pitch": -0.07412154964760376,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1068221031909813,
+          "pitch": 0.02989552226753034,
+          "rotation": 0,
+          "target": "0-welcome-to-ban-mae-hat"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.3781348684967067,
+          "pitch": -0.036269890144325245,
+          "title": "Terrain for growing crops",
+          "text": "Karen communities in the upland regions of northern Thailand have long adapted their agricultural practices to rugged, forested terrain. They understanding the interplay between topography, soils, and land-management."
+        },
+        {
+          "yaw": 2.1036197197866207,
+          "pitch": -0.0883422323368741,
+          "title": "House and weaving center",
+          "text": "In Karen hill-tribe villages, the <strong data-start=\"98\" data-end=\"107\">house</strong> and the <strong data-start=\"116\" data-end=\"134\">weaving center</strong> stand as focal points of domestic life, social cohesion, and cultural identity. Their architectural forms and communal functions reflect traditional knowledge, local materials, and the evolving needs of the community."
+        }
+      ]
+    },
+    {
+      "id": "7-village1_irrigation",
+      "name": "village1_Irrigation",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 384,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Karen Hill Tribe 360° View",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
